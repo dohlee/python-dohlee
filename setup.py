71b4d282f126ef6aae383861a046f438bfe6972b
dohlee <apap950419@gmail.com>
2018-04-22 22:35:13 +0900

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def read(*names, **kwargs):
 
 setup(
     name='dohlee',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT license',
     description='My personal python utility library.',
     long_description='%s\n%s' % (
