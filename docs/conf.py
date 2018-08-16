d575d63ab47cd2d51ac83430c612f6a8ac76cee8
dohlee <apap950419@gmail.com>
2018-08-16 21:24:45 +0900

    Add docs/requirements.txt.

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, '/mnt/c/Users/user/Desktop/code/python-dohlee/src/dohlee')
+import sys
+sys.path.insert(0, '../src/dohlee')
 
 
 # -- Project information -----------------------------------------------------
