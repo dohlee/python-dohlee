dffde91f2bc0e7962e1687573e28b43a20c187f0
dohlee <apap950419@gmail.com>
2018-08-16 22:50:01 +0900

    Use freesans font.

--- src/dohlee/plot.py
+++ src/dohlee/plot.py
@@ -12,7 +12,8 @@ from matplotlib.lines import Line2D
 from collections import Counter
 from functools import wraps
 
-def get_ax_to_draw(ax, figsize=None):
+
+def _get_ax_to_draw(ax, figsize=None):
     """If ax is not specified, return an axis to draw a plot.
     Otherwise, return ax.
     """
@@ -24,7 +25,7 @@ def get_ax_to_draw(ax, figsize=None):
         return ax
 
 
-def try_save(file, dpi=150):
+def _try_save(file, dpi=150):
     """If file is specified, save the figure to the file with given resolution in dpi.
     Otherwise, show the figure.
     """
@@ -34,20 +35,19 @@ def try_save(file, dpi=150):
 def save(file, dpi=120, tight_layout=True):
     """Save plot to a file.
 
-    Attributes:
-        file (str): Path to the resulting image file.
-        dpi (int, default=120): Resolution.
-        tight_layout (bool, default=True): Whether to run plt.tight_layout() before saving the plot.
+    :param str file: Path to the resulting image file.
+    :param int dpi: (default=120) Resolution.
+    :param bool tight_layout: (default=True) Whether to run plt.tight_layout() before saving the plot.
     """
     if tight_layout:
         plt.tight_layout()
     plt.savefig(file, dpi=dpi)
 
 
-def my_plot(func):
+def _my_plot(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        ax = get_ax_to_draw(kwargs.get('ax', None), kwargs.get('figsize', None))
+        ax = _get_ax_to_draw(kwargs.get('ax', None), kwargs.get('figsize', None))
         if 'ax' in kwargs:
             del kwargs['ax']
         if 'figsize' in kwargs:
@@ -70,12 +70,14 @@ def my_plot(func):
             del kwargs['ylabel']
 
         result = func(*args, ax=ax, **kwargs)
-        try_save(kwargs.get('file', None))
+        _try_save(kwargs.get('file', None))
     return wrapper
 
 
 # Set plot preference which looks good to me.
 def set_style(style='white', palette='deep', context='talk', font='Helvetica Neue', font_scale=1.25, rcparams={'figure.figsize': (11.7, 8.27)}):
+    """Set plot preference in a way that looks good to me.
+    """
     sns.set(style=style,
             palette=palette,
             context=context,
@@ -84,7 +86,7 @@ def set_style(style='white', palette='deep', context='talk', font='Helvetica Neu
             rc=rcparams)
 
 
-@my_plot
+@_my_plot
 def frequency(data, order=None, sort_by_values=False, ax=None, **kwargs):
     """Plot frequency bar chart.
 
@@ -107,7 +109,7 @@ def frequency(data, order=None, sort_by_values=False, ax=None, **kwargs):
         assert set(order) == set(counter.keys()), 'The order must contain all the elements.'
     counts = [counter[key] for key in order]
 
-    ax = get_ax_to_draw(ax)
+    ax = _get_ax_to_draw(ax)
 
     # Some parameters used for plot configuration.
     height = max(counts) * 1.167
@@ -135,29 +137,41 @@ def frequency(data, order=None, sort_by_values=False, ax=None, **kwargs):
                 ha='center')
 
 
-@my_plot
+@_my_plot
 def histogram(data, ax=None, **kwargs):
+    """Draw a histogram.
+
+    >>> histogram(data=data, ax=ax, lw=1.55)
+
+    :param list data: A list containing values.
+        Density of the values will be drawn as a histogram.
+    :param axis ax: Matplotlib axis to draw the plot on.
+    """
     plt.hist(data, color='black', ec='white', lw=1.33, **kwargs)
 
 
-@my_plot
+@_my_plot
 def volcano(data, x, y, padj, label, cutoff=0.05, sample1=None, sample2=None, ax=None):
     """Draw a volcano plot.
 
-    Examples:
-        volcano(data=data, x='log2FoldChange', y='pvalue', label='Gene_Symbol', cutoff=0.05, padj='padj', figsize=(10.8, 8.4))
+    >>> volcano(data=data,
+                x='log2FoldChange',
+                y='pvalue',
+                label='Gene_Symbol',
+                cutoff=0.05,
+                padj='padj',
+                figsize=(10.8, 8.4))
 
-    Attributes:
-        data (pandas dataframe): A dataframe resulting from DEG-discovery tool.
-        x (str): Column name denoting log2 fold change.
-        y (str): Column name denoting p-value.
-                 (Note that p-values will be log10-transformed, so they should not be transformed beforehand.)
-        padj (str): Column name denoting adjusted p-value.
-        label (str): Column name denoting gene identifier.
-        cutoff (float, optional): Adjusted p-value cutoff value to report significant DEGs.
-        sample1 (str, optional): First sample name.
-        sample2 (str, optional): Second sample name.
-        ax (pyplot axis): Axis to draw the plot.
+    :param dataframe data: A dataframe resulting from DEG-discovery tool.
+    :param str x: Column name denoting log2 fold change.
+    :param str y: Column name denoting p-value.
+        (Note that p-values will be log10-transformed, so they should not be transformed beforehand.)
+    :param str padj: Column name denoting adjusted p-value.
+    :param str label: Column name denoting gene identifier.
+    :param float cutoff: (Optional) Adjusted p-value cutoff value to report significant DEGs.
+    :param str sample1: (Optional) First sample name.
+    :param str sample2: (Optional) Second sample name.
+    :param axis ax: (Optional) Matplotlib axis to draw the plot on.
     """
     # Set x and y extent.
     x_limit = max(-np.min(data[x].values), np.max(data[x].values))
@@ -184,8 +198,16 @@ def volcano(data, x, y, padj, label, cutoff=0.05, sample1=None, sample2=None, ax
     ax.set_ylabel(r'$log_{10}$(p-value)')
 
 
+@_my_plot
 def pca(data, labels=None, ax=None, **kwargs):
-    '''Draw a simple principle component analysis plot of the data.'''
+    '''Draw a simple principle component analysis plot of the data.
+
+    :param matrix data: Input data. Numpy array recommended.
+    :param list labels: (Optional) Corresponding labels to each datum.
+        If specified, data points in the plot will be colored according to the label.
+    :param axis ax: (Optional) Matplotlib axis to draw the plot on.
+    :param kwargs: Any other keyword arguments will be passed onto matplotlib.pyplot.scatter.
+    '''
     # Fit PCA and get pc's
     pca = PCA(n_components=2)
     pca.fit(data)
@@ -200,7 +222,7 @@ def pca(data, labels=None, ax=None, **kwargs):
         for label in set(labels):
             toDraw = (labels == label)  # only draw these points this time
 
-            plt.scatter(x=pc[toDraw, 0], y=pc[toDraw, 1], label=label)
+            plt.scatter(x=pc[toDraw, 0], y=pc[toDraw, 1], label=label, **kwargs)
             plt.legend(loc='best')
 
     # show explained variance ratio in the plot axes
