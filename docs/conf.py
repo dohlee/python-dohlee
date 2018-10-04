6c44d68f66db6d69cf57fb8ba6337d1f74344fbd
dohlee <apap950419@gmail.com>
2018-10-04 17:08:36 +0900

    Bump version: 0.1.15 → 0.1.16

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2018, Dohoon Lee'
 author = 'Dohoon Lee'
 
 # The full version, including alpha/beta/rc tags
-version = release = '0.1.15'
+version = release = '0.1.16'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ def read(*names, **kwargs):
 
 setup(
     name='dohlee',
-    version='0.1.15',
+    version='0.1.16',
     license='MIT license',
     description='My personal python utility library.',
     # long_description='%s\n%s' % (
