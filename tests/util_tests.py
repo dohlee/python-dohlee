be8069e6c19ad97754911632609eb3f1fc088f54
dohlee <apap950419@gmail.com>
2018-10-11 14:39:08 +0900

    Fix tests for split_ext.

--- tests/util_tests.py
+++ tests/util_tests.py
@@ -1,11 +1,11 @@
 import dohlee.util as util
 
 
-def given_filename_with_single_dot_splitext_test():
+def given_filename_with_single_dot_split_ext_test():
     filename = '/tmp/a/b/c/d/e/filename.png'
-    assert util.stripext(filename) == 'filename'
+    assert util.strip_ext(filename) == 'filename'
 
 
-def given_filname_with_multiple_dots_splitext_test():
+def given_filname_with_multiple_dots_split_ext_test():
     filename = '/tmp/a/b/c/d/e/f.i.l.e.n.a.m.e.png'
-    assert util.stripext(filename) == 'f.i.l.e.n.a.m.e'
+    assert util.strip_ext(filename) == 'f.i.l.e.n.a.m.e'
