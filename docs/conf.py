bc165eca007bb1809f6c0a8362e1c8437ae8a084
dohlee <dohlee.bioinfo@gmail.com>
2019-10-09 00:12:39 +0900

    Bump version: 0.1.19 → 0.1.20

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2018, Dohoon Lee'
 author = 'Dohoon Lee'
 
 # The full version, including alpha/beta/rc tags
-version = release = '0.1.19'
+version = release = '0.1.20'
 
 
 # -- General configuration ---------------------------------------------------
